1b8910e3691aff0cb3b52df76d4fdc100e1e199b
Alex Hildebert <navytuna@users.noreply.github.com>
2017-11-03 11:27:08 -0400

    Fixing the Quarterback class
    Changed the comment, fixed the structure, implemented the equations, and implemented the getQrating method

--- src/Quarter.java
+++ src/Quarter.java
@@ -1,3 +1,4 @@
+//Things in this class are not named as in the other classes
 
 public class Quarter {
  private double completions, attempts, yards, touchdowns, interceptions;
@@ -9,45 +10,75 @@ public class Quarter {
  interceptions=interceptions1;
  }
  public double getA(){
-	 if(((completions/attempts-0.3)*5)<2.375){
-		 if(((completions/attempts-0.3)*5)>0)
-	 return((completions/attempts-0.3)*5);
-		 else
-			 return 0.0;
+	 double A = (completions/attempts-0.3)*5;
+	 if (A < 2.375){
+		 if (0 < A){
+			 return A;
+		 }else{
+			 A = 0;
+			 return A;
+		 }
+	 }else{
+		 A = 2.375;
+		 return A;
 	 }
-	 else
-		 return 2.375;
  }
+ 
  public double getB(){
-	 if(((completions/attempts-0.3)*5)<2.375){
-		 if(((completions/attempts-0.3)*5)>0)
-	 return((completions/attempts-0.3)*5);
-		 else
-			 return 0.0;
+	 double B = (yards/attempts-3)*0.25;
+	 if (B < 2.375){
+		 if (0 < B){
+			 return B;
+		 }else{
+			 B = 0;
+			 return B;
+		 }
+	 }else{
+		 B=2.375;
+		 return B;
 	 }
-	 else
-		 return 2.375;
  }
+ 
  public double getC(){
-	 if(((completions/attempts-0.3)*5)<2.375){
-		 if(((completions/attempts-0.3)*5)>0)
-	 return((completions/attempts-0.3)*5);
-		 else
-			 return 0.0;
+	 double C = (touchdowns/attempts)*20;
+	 if (C < 2.375){
+		 if (0 < C){
+			 return C;
+		 }else{
+			 C = 0;
+			 return C;
+		 }
+	 }else{
+		 C = 2.375;
+		 return C;
 	 }
-	 else
-		 return 2.375;
  }
+ 
  public double getD(){
-	 if(((completions/attempts-0.3)*5)<2.375){
-		 if(((completions/attempts-0.3)*5)>0)
-	 return((completions/attempts-0.3)*5);
-		 else
-			 return 0.0;
+	 double D = 2.375 - (interceptions/attempts*25);
+	 if (D < 2.375){
+		 if (0 < D){
+			 return D;
+		 }else{
+			 D = 0;
+			 return D;
+		 }
+	 }else{
+		 D = 2.375;
+		 return D;
 	 }
-	 else
-		 return 2.375;
  }
+<<<<<<< HEAD
+ 
+ public double getQrating (double A, double B, double C, double D) {
+	double rating = ((A+B+C+D)/6)*100;
+	return rating;
+=======
+ public double getRating(){
+	 return((getA()+getB()+getC()+getD())/6)*100;
  }
+>>>>>>> 81a196cf7cb6d359e42b6c995a5737d9316d9597
+ }
+}
 
 
